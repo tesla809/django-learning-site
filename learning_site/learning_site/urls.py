13d13967295c94c717a98208817e5dcf70a0e8c5
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-12-15 19:01:53 -0500

    added url to url.py to include our hello_world view/function in views.py aka views.hello_word.

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -1,4 +1,4 @@
-"""learning_site URL Configuration
+"""learning_site URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -15,6 +15,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from learning_site import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.hello_world),
 ]
