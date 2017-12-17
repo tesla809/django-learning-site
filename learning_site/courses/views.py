964bb23dad713801f35048d85fa8675c9b2aefeb
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-12-17 01:42:12 -0500

    added course_list view to course/views.py. Returns simple string of all courses in our db.

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,3 +1,13 @@
+"""Views for course app."""
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from courses.models import Course
+
+
 # Create your views here.
+def course_list(request):
+    """Return simple course list."""
+    courses = Course.objects.all()
+    output = ', '.join([str(course) for course in courses])
+    return HttpResponse(output)
