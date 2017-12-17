17cc6d10021ca3be4a99a3b0ad10269e22e5a8b1
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-12-16 19:15:18 -0500

    Modified models.py in our courses app. Added our Course Model.

--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -1,3 +1,11 @@
+"""Models for courses app."""
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    """Course Model to hold our various courses."""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
